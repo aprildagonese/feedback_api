2cd88327c40b5ec2d60e278ccc7612041b7b5490
April Dagonese <adagonese@gmail.com>
2019-05-20 14:13:50 -0600

    Changes cohort refresh

--- lib/feedback_api_web/facades/users_update_facade.ex
+++ lib/feedback_api_web/facades/users_update_facade.ex
@@ -4,12 +4,8 @@ defmodule FeedbackApiWeb.UsersUpdateFacade do
   import Services.Rooster
 
   def update_student_data do
-    try do
-      update_cohorts()
-      update_students()
-    rescue
-      e -> "Something went wrong."
-    end
+    update_cohorts()
+    update_students()
   end
 
   def update_cohorts do
@@ -29,20 +25,21 @@ defmodule FeedbackApiWeb.UsersUpdateFacade do
   def refresh_cohort(cohort) do
     name = cohort["attributes"]["name"]
     status = cohort["attributes"]["status"]
-    result = case Cohort.get(Cohort, name) do
+    result = case Repo.get_by(Cohort, %[name: name]) do
                nil  -> %Cohort{name: name, status: status} # Cohort not found, we build one
                cohort -> cohort          # Cohort exists, let's update it
              end
-             |> Cohort.changeset(changes)
+             |> Cohort.changeset()
              |> Repo.insert_or_update
 
     case result do
-      {:ok, model}        -> # Inserted or updated with success
-      {:error, changeset} -> # Something went wrong
+      {:ok, model}        ->  {:ok, model}
+      {:error, changeset} ->  {:error, changeset.errors}
     end
   end
 
   def refresh_student(student) do
+    name = student["attributes"]
   end
 
 end
