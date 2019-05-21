f7fc58ab326ed1dc56b65c5c4198bfc2da33e7aa
April Dagonese <adagonese@gmail.com>
2019-05-21 12:16:34 -0600

    Changes DB pool size

--- config/prod.exs
+++ config/prod.exs
@@ -20,7 +20,7 @@ config :feedback_api, FeedbackApiWeb.Endpoint,
 config :feedback_api, FeedbackApi.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "18"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
 # Do not print debug messages in production
