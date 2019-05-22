d654dfbfbd62fa2a924cb0bb5792cc04a6d00cc0
April Dagonese <adagonese@gmail.com>
2019-05-22 17:30:04 -0600

    Updates Surveys schema to belong_to user_id

--- lib/feedback_api/user.ex
+++ lib/feedback_api/user.ex
@@ -13,6 +13,7 @@ defmodule FeedbackApi.User do
     belongs_to :cohort, FeedbackApi.Cohort
     has_many :responses, FeedbackApi.Response, foreign_key: :response_user
     has_many :ratings, FeedbackApi.Response, foreign_key: :target_user
+    has_many :surveys, FeedbackApi.Survey
     many_to_many :groups, FeedbackApi.Group, join_through: FeedbackApi.GroupMember
 
     timestamps()
