e31a3d6adbf03bcccabefb500064df44332027c9
plapicola <p.lapicola@gmail.com>
2019-05-25 15:32:19 -0600

    Adds averages for user

--- lib/feedback_api_web/controllers/surveys/student_average_controller.ex
+++ lib/feedback_api_web/controllers/surveys/student_average_controller.ex
@@ -8,11 +8,12 @@ defmodule FeedbackApiWeb.Surveys.StudentAverageController do
         conn |> put_status(:unauthorized) |> json(%{error: "Invalid API Key"})
 
       user ->
-        conn
-        |> render("show.json", %{
+        data = %{
           average: Survey.average_for_user(params["survey_id"], user),
           survey: Survey.one(params["survey_id"])
-          })
+        }
+        conn
+        |> render("show.json", %{average: data})
     end
   end
 end
---
+++ lib/feedback_api_web/views/surveys/student_average_view.ex
@@ -0,0 +1,23 @@
+defmodule FeedbackApiWeb.Surveys.StudentAverageView do
+  use FeedbackApiWeb, :view
+  alias FeedbackApiWeb.SurveyView
+  alias FeedbackApiWeb.Surveys.StudentAverageView
+
+  def render("show.json", %{average: average}) do
+    render_one(average, StudentAverageView, "student_average.json")
+  end
+
+  def render("student_average.json", %{student_average: student_average}) do
+    %{
+      survey: render_one(student_average.survey, SurveyView, "survey.json"),
+      averages:
+        Enum.map(student_average.average, fn student ->
+          %{
+            user_id: student.user_id,
+            question_id: student.question_id,
+            average_rating: student.average_rating
+          }
+        end)
+    }
+  end
+end
