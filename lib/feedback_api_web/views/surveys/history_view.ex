b47f2e74144a112e66059f9e874a3df54101a375
plapicola <p.lapicola@gmail.com>
2019-05-27 11:28:18 -0600

    Adds survey history view

---
+++ lib/feedback_api_web/views/surveys/history_view.ex
@@ -0,0 +1,9 @@
+defmodule FeedbackApiWeb.Surveys.HistoryView do
+  use FeedbackApiWeb, :view
+  alias FeedbackApiWeb.SurveyView
+
+  def render("index.json", %{surveys: surveys}) do
+    render_many(surveys, SurveyView, "survey.json")
+  end
+
+end
