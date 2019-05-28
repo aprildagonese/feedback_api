c8885ae42163395efab7170b820a8c271b5a9b6d
plapicola <p.lapicola@gmail.com>
2019-05-28 14:09:48 -0600

    Updates mailer service to offer multiple mail endpoints

--- lib/services/mailer.ex
+++ lib/services/mailer.ex
@@ -1,9 +1,21 @@
 defmodule Services.Mailer do
-  def send_notifications(users) do
-    HTTPoison.start()
+  def send_survey_notifications(users) do
     body = Poison.encode!(users)
+    url = "http://mail.turingfeedback.com/api/v1/new_survey"
 
-    url = "http://mail.turingfeedback.com/api/v1/messages"
+    send_request(body, url)
+  end
+
+  def send_welcome_notification(user) do
+    body = Poison.encode!(user)
+    url = "http://mail.turingfeedback.com/api/v1/welcome"
+
+    send_request(body, url)
+  end
+
+  defp send_request(body, url) do
+    HTTPoison.start()
+
     headers = [{"Content-type", "application/json"}]
     options = [recv_timeout: 40_000, timeout: 40_000]
 
