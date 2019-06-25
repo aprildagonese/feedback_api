3b10082a1d4b437135941ade37cc92cace1ac2d1
plapicola <p.lapicola@gmail.com>
2019-06-24 22:48:12 -0600

    Adds test to verify ordering returned by endpoint

--- test/feedback_api_web/controllers/closed_student_surveys_test.exs
+++ test/feedback_api_web/controllers/closed_student_surveys_test.exs
@@ -56,14 +56,36 @@ defmodule FeedbackApiWeb.ClosedStudentSurveysTest do
     Ecto.build_assoc(question_2, :answers, %{description: "One", value: 1})
     |> Repo.insert!()
 
+    survey_3 =
+      Ecto.build_assoc(user, :surveys, %Survey{name: "Closed Survey Two", status: 1})
+      |> Repo.insert!()
+      |> Repo.preload([:groups, :questions])
+
+    group_3 =
+      Ecto.build_assoc(survey_3, :groups, %{name: "Test"})
+      |> Repo.insert!()
+      |> Repo.preload([:users, :survey])
+
+    Ecto.Changeset.put_assoc(Ecto.Changeset.change(group_3), :users, [user, user_2])
+    |> Repo.update!()
+
+    question_3 =
+      Ecto.build_assoc(survey_3, :questions, %{text: "Pick a number between one and four"})
+      |> Repo.insert!()
+      |> Repo.preload(:answers)
+
+    Ecto.build_assoc(question_3, :answers, %{description: "One", value: 1})
+    |> Repo.insert!()
+
     :ok
   end
 
   test "Returns closed surveys where the user was a participant", %{conn: conn} do
-    [_, survey] = Repo.all(Survey) |> Repo.preload([:groups, :questions])
+    [_, survey_2, survey_3] = Repo.all(Survey) |> Repo.preload([:groups, :questions])
     [user, user_2] = Repo.all(User)
-    [_, question] = Repo.all(Question) |> Repo.preload(:answers)
-    [answer] = question.answers
+    [_, question_1, question_2] = Repo.all(Question) |> Repo.preload(:answers)
+    [answer_1] = question_1.answers
+    [answer_2] = question_2.answers
 
     uri = "/api/v1/surveys/closed?api_key=#{user.api_key}"
 
@@ -71,18 +93,49 @@ defmodule FeedbackApiWeb.ClosedStudentSurveysTest do
 
     expected = [
       %{
-        "id" => survey.id,
-        "surveyName" => survey.name,
+        "id" => survey_3.id,
+        "surveyName" => survey_3.name,
         "surveyExpiration" => nil,
-        "created_at" => NaiveDateTime.to_iso8601(survey.inserted_at),
-        "updated_at" => NaiveDateTime.to_iso8601(survey.updated_at),
+        "created_at" => NaiveDateTime.to_iso8601(survey_3.inserted_at),
+        "updated_at" => NaiveDateTime.to_iso8601(survey_3.updated_at),
         "status" => "Closed",
         "questions" => [
           %{
-            "id" => question.id,
+            "id" => question_2.id,
             "questionTitle" => "Pick a number between one and four",
             "options" => [
-              %{"description" => "One", "pointValue" => 1, "id" => answer.id}
+              %{"description" => "One", "pointValue" => 1, "id" => answer_2.id}
+            ]
+          }
+        ],
+        "groups" => [
+          %{
+            "members" => [
+              %{
+                "id" => user_2.id,
+                "name" => user_2.name,
+                "cohort" => "1811",
+                "program" => "B",
+                "status" => "Active"
+              }
+            ],
+            "name" => "Test"
+          }
+        ]
+      },
+      %{
+        "id" => survey_2.id,
+        "surveyName" => survey_2.name,
+        "surveyExpiration" => nil,
+        "created_at" => NaiveDateTime.to_iso8601(survey_2.inserted_at),
+        "updated_at" => NaiveDateTime.to_iso8601(survey_2.updated_at),
+        "status" => "Closed",
+        "questions" => [
+          %{
+            "id" => question_1.id,
+            "questionTitle" => "Pick a number between one and four",
+            "options" => [
+              %{"description" => "One", "pointValue" => 1, "id" => answer_1.id}
             ]
           }
         ],
