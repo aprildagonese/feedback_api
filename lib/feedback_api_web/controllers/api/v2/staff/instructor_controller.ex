4a066d6f1095aa62cb29fc812c5c6cc2d9ec0240
plapicola <p.lapicola@gmail.com>
2019-07-07 10:39:25 -0600

    Runs formatter

--- lib/feedback_api_web/controllers/api/v2/staff/instructor_controller.ex
+++ lib/feedback_api_web/controllers/api/v2/staff/instructor_controller.ex
@@ -1,16 +1,16 @@
 defmodule FeedbackApiWeb.Api.V2.Staff.InstructorController do
-   use FeedbackApiWeb, :controller
+  use FeedbackApiWeb, :controller
 
-   alias FeedbackApi.User
+  alias FeedbackApi.User
 
-   def index(conn, %{"api_key" => api_key}) do
-      case User.authorize_instructor(api_key) do
-         nil -> conn |> put_status(:unauthorized) |> json(%{error: "Invalid API Key"})
-         user -> conn |> render("index.json", %{instructors: User.active_instructors(user)})
-      end
-   end
+  def index(conn, %{"api_key" => api_key}) do
+    case User.authorize_instructor(api_key) do
+      nil -> conn |> put_status(:unauthorized) |> json(%{error: "Invalid API Key"})
+      user -> conn |> render("index.json", %{instructors: User.active_instructors(user)})
+    end
+  end
 
-   def index(conn, _params) do
-      conn |> put_status(:unauthorized) |> json(%{error: "Invalid API Key"})
-   end
+  def index(conn, _params) do
+    conn |> put_status(:unauthorized) |> json(%{error: "Invalid API Key"})
+  end
 end
--- lib/feedback_api_web/views/api/v2/staff/instructor_view.ex
+++ lib/feedback_api_web/views/api/v2/staff/instructor_view.ex
@@ -1,16 +1,16 @@
 defmodule FeedbackApiWeb.Api.V2.Staff.InstructorView do
-   use FeedbackApiWeb, :view
+  use FeedbackApiWeb, :view
 
-   alias FeedbackApiWeb.Api.V2.Staff.InstructorView
+  alias FeedbackApiWeb.Api.V2.Staff.InstructorView
 
-   def render("index.json", %{instructors: instructors}) do
-      render_many(instructors, InstructorView, "instructor.json")
-   end
+  def render("index.json", %{instructors: instructors}) do
+    render_many(instructors, InstructorView, "instructor.json")
+  end
 
-   def render("instructor.json", %{instructor: instructor}) do
-      %{
-         id: instructor.id,
-         name: instructor.name
-      }
-   end
+  def render("instructor.json", %{instructor: instructor}) do
+    %{
+      id: instructor.id,
+      name: instructor.name
+    }
+  end
 end
